f135d9f1cfa7e5a331af213fa31e6b439b08204d
Mauricio Tadeuferrite <mauricio.tadeuferrite@ericsson.com>
2022-02-02 08:49:14 +0000

    useState implementation

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const title = props.title;
-  const amount = props.amount;
+  const [title, setTitle] = useState(props.title);
+  
+  const clickHandler = () => {
+    
+    setTitle('Update!!');
+    console.log(title);
+  }
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <h2 className='expense-item__price'>${amount}</h2>
+        <h2 className='expense-item__price'>${props.amount}</h2>
       </div>
-    </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 }
 
